1309fc325e21866118a21a227bd78e60c707480f
Preston Cropp <pkcropp@gmail.com>
2018-03-23 16:17:10 -0500

    changed port definition

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(bodyParser.json());
 // Routes
 // =============================================================
 
-// require("./app/routing/apiRoutes.js")(app);
-// require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-app.get('/', function(req,res){
-  res.send('hello');
-});
+//app.get('/', function(req,res){
+ // res.send('hello');
+//});
 
 // Starts the server to begin listening
 // =============================================================

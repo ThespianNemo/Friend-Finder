a1e29122ea8c8e1e6b42f1873a67a420b4abb9c9
Preston Cropp <pkcropp@gmail.com>
2018-03-23 16:09:37 -0500

    changed require

--- server.js
+++ server.js
@@ -23,6 +23,4 @@ app.get('/', function(req,res){
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  }); 
+app.listen(PORT);
